f7f6e0b8358ea0df101c4c5b6f5cc2fc0aada469
h-behgam <h.behgam@gmail.com>
2022-02-04 10:57:34 +0100

    Add some features
    add product, info box, introduction box,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,63 @@
-(function() {
-    var navToggleBtn = document.querySelector('.nav-toggle');
-    var navList = document.querySelector('.header-nav .nav-list');
+// hamburger menu
+let nav = document.getElementById("tNav");
+let ul = document.getElementById("navList");
+function menu() {
+  ul.classList.toggle("opened");
+}
+nav.addEventListener("click", menu);
 
-    // toggle list on click
-    navToggleBtn.onclick = function(e) {
-        e.stopPropagation();
-        navList.classList.toggle('opened');
+// hero slider
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// product slider
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Get image from api for hero slider
+let slider = document.getElementById("sliders");
+// console.log(slider);
+// slider.addEventListener('load', sliderImage);
+function sliderImage() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://coffee.alexflipnote.dev/random.json", true);
+  xhttp.onload = function () {
+    if (this.status == 200) {
+      let image = JSON.parse(this.responseText);
+      var output = "";
+      for (let i = 0; i < 3; i++) {
+        output += `
+            <div class="swiper-slide"><img src="${image.file}" alt=""></div>
+        `;
+      }
+      document.getElementById("sWrapper").innerHTML = output;
+      console.log("The request succeeded.");
+    } else if (this.status == 404 || this.status == 403) {
+      console.log("Error 403 OR 404");
+    } else {
+      console.log("Error is: " + this.status);
     }
+  };
+  xhttp.send();
+}
+sliderImage();
 
-    // close list on outside click
-    window.addEventListener("click", closeNavList);
-    function closeNavList() {
-        navList.classList.remove('opened');
-    }
-})()
-
-
-// window.addEventListener('scroll', function(){ 
+// window.addEventListener('scroll', function(){
 //     //Here you forgot to update the value
 //     scrollpos = window.scrollY;
 
